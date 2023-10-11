de90c25087c8053cd726352fb58f71cb2830258f
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2023-10-11 11:51:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 resource "aws_instance" "demo_instance" {
   count         = 3
-  ami           = "ami-0ded8326293d3201b" 
+  ami           = "ami-0f5ee92e2d63afc18" 
   instance_type = "t2.micro"
 }
