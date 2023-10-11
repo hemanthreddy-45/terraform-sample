07f5c232f6a4a8e7d229b6bfb6fd00eff0b5d0ad
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2023-10-11 13:14:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 provider "aws" {
   region = "us-east-2" 
 }
-
+data "aws_vpc" "my_vpc" {
+  id = "vpc-02300b005147bb372"  # Replace with the desired VPC ID
+}
 resource "aws_instance" "demo_instance" {
   count         =  3
   ami           = "ami-024e6efaf93d85776" 
