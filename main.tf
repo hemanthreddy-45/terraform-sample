05d37170e828c1d4881b9998be68c7e2af0ea956
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2023-10-11 13:19:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,12 @@ provider "aws" {
 data "aws_vpc" "my_vpc" {
   id = "vpc-02300b005147bb372"  # Replace with the desired VPC ID
 }
+data "aws_subnet" "my_subnet" {
+  id = "subnet-028ccb0b988ef558e"  # Replace with the desired subnet ID
+}
 resource "aws_instance" "demo_instance" {
-  count         =  3
+subnet_id = data.aws_subnet.my_subnet.id 
+count         =  3
   ami           = "ami-024e6efaf93d85776" 
   instance_type = "t2.micro"
 }
