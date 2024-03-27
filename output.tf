779dbce7020f58db1c1d82e2886f775d902d9bae
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-03-27 12:20:48 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,14 +2,16 @@ output "instance_info" {
   description = "Information about the EC2 instances"
   value = {
     instance_ids = aws_instance.demo_instance[*].id
-    public_ips  = aws_instance.demo_instance[*].public_ip
-    private_ips = aws_instance.demo_instance[*].private_ip
+    public_ips   = aws_instance.demo_instance[*].public_ip
+    private_ips  = aws_instance.demo_instance[*].private_ip
   }
+}
+
 output "bucket_info" {
   description = "Information about the S3 bucket"
   value = {
-    bucket_id = aws_s3_bucket.my_bucket.id
-    bucket_arn = aws_s3_bucket.my_bucket.arn
+    bucket_id     = aws_s3_bucket.my_bucket.id
+    bucket_arn    = aws_s3_bucket.my_bucket.arn
     bucket_region = aws_s3_bucket.my_bucket.region
   }
 }
