d6ca6916fd13cad144a4444914daa1054eca4ca6
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-03-27 10:13:42 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -5,4 +5,11 @@ output "instance_info" {
     public_ips  = aws_instance.demo_instance[*].public_ip
     private_ips = aws_instance.demo_instance[*].private_ip
   }
+output "bucket_info" {
+  description = "Information about the S3 bucket"
+  value = {
+    bucket_id = aws_s3_bucket.my_bucket.id
+    bucket_arn = aws_s3_bucket.my_bucket.arn
+    bucket_region = aws_s3_bucket.my_bucket.region
+  }
 }
