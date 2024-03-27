32b0773a4dcda54b32dc412a32bb21753ff215ba
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-03-27 10:11:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,22 @@
 provider "aws" {
-  region = "us-east-2" 
+  region = "us-east-2"
 }
+
 data "aws_vpc" "my_vpc" {
   id = "vpc-02300b005147bb372"  # Replace with the desired VPC ID
 }
+
 data "aws_subnet" "my_subnet" {
   id = "subnet-028ccb0b988ef558e"  # Replace with the desired subnet ID
 }
+
 resource "aws_instance" "demo_instance" {
-subnet_id = data.aws_subnet.my_subnet.id 
-count         =  1
-  ami           = "ami-024e6efaf93d85776" 
+  subnet_id     = data.aws_subnet.my_subnet.id
+  count         = 1
+  ami           = "ami-024e6efaf93d85776"
   instance_type = "t2.micro"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "opsera-terraform-bucket-tf"  # Replace with your desired bucket name
 }
